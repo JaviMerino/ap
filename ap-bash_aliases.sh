ae0452ae2b34dfbc41df80ff30f2a7d5d1ac6a1b
Javi Merino (Vicho) <cibervicho@gmail.com>
2010-03-16 15:47:35 +0100

    added example of aliases for bash

---
+++ ap-bash_aliases.sh
@@ -0,0 +1,4 @@
+alias ll='ap -p "/usr/bin/less -FR" -a --color=always ls -lArth'
+alias ps='ap -p "/usr/bin/less -F" /bin/ps'
+alias grep='ap -p "/usr/bin/less -FR" -a --color=always /bin/grep'
+alias egrep='ap -p "/usr/bin/less -FR" -a --color=always /bin/egrep'
